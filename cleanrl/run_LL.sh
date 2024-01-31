71a033b5f30501e65e463c6db5a548d6ac8d4aa5
x4nnon <x4nnon@gmail.com>
2024-01-31 19:40:01 +0000

    LL max ep length and step handler

--- cleanrl/run_LL.sh
+++ cleanrl/run_LL.sh
@@ -3,22 +3,22 @@
 # hyperparam sweeps for our methods 
 
 
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 
 
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
 
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 
 
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
 
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=0 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=1 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
-python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 1 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 2 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 3 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 4 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 &
+python3 $(pwd)/cleanrl/cleanrl/ppo.py --seed 5 --env-id MetaGridEnv/metagrid-v0 --current_depth=2 --wandb_project_name LL_seed_comparisons --env_id  LunarLander-v2 --total_timesteps=600000 
